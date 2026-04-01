9e58afb66da20c6d13ae53d2b0ca8a9934d0ab62
CarlosCervante5 <64617749+CarlosCervante5@users.noreply.github.com>
2026-04-01 12:49:49 -0600

    fix: revert tsconfig.app.json to match working branch, add sandbox env properties

--- src/environments/environment.sandbox.ts
+++ src/environments/environment.sandbox.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://sandbox.bmwvecsahidalgo.com/vecsa-backend'
+  baseUrl: 'https://sandbox.bmwvecsahidalgo.com/vecsa-backend',
+  stripePublishableKey: 'pk_test_xxx',
+  tidioProjectId: ''
 };
