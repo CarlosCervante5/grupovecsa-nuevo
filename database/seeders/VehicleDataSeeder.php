71c1bb5c23c30a6f07040eb55248d4134cb91c98
CarlosCervante5 <64617749+CarlosCervante5@users.noreply.github.com>
2026-04-01 16:39:50 -0600

    fix: use firstOrCreate in VehicleDataSeeder to prevent duplicate errors on re-run

--- database/seeders/VehicleDataSeeder.php
+++ database/seeders/VehicleDataSeeder.php
@@ -8,103 +8,40 @@ use App\Models\BrandLine;
 use App\Models\LineModel;
 use App\Models\VehicleBody;
 use App\Models\Dealership;
-use App\Models\Vehicle;
 
 class VehicleDataSeeder extends Seeder
 {
     public function run()
     {
-        // Crear marcas
-        $bmw = VehicleBrand::create([
-            'name' => 'BMW',
-            'image_path' => 'bmw-logo.png'
-        ]);
+        // Marcas
+        $bmw = VehicleBrand::firstOrCreate(['name' => 'BMW'], ['image_path' => 'bmw-logo.png']);
+        $mini = VehicleBrand::firstOrCreate(['name' => 'MINI'], ['image_path' => 'mini-logo.png']);
+        $motorrad = VehicleBrand::firstOrCreate(['name' => 'BMW Motorrad'], ['image_path' => 'motorrad-logo.png']);
+        $chevrolet = VehicleBrand::firstOrCreate(['name' => 'Chevrolet'], ['image_path' => 'chevrolet-logo.png']);
+        $gmc = VehicleBrand::firstOrCreate(['name' => 'GMC'], ['image_path' => 'gmc-logo.png']);
 
-        $mini = VehicleBrand::create([
-            'name' => 'MINI',
-            'image_path' => 'mini-logo.png'
-        ]);
+        // Líneas
+        $x3Line = BrandLine::firstOrCreate(['name' => 'X3', 'brand_id' => $bmw->id]);
+        $x4Line = BrandLine::firstOrCreate(['name' => 'X4', 'brand_id' => $bmw->id]);
+        $cooperLine = BrandLine::firstOrCreate(['name' => 'Cooper', 'brand_id' => $mini->id]);
+        $trackerLine = BrandLine::firstOrCreate(['name' => 'Tracker', 'brand_id' => $chevrolet->id]);
 
-        $motorrad = VehicleBrand::create([
-            'name' => 'BMW Motorrad',
-            'image_path' => 'motorrad-logo.png'
-        ]);
+        // Modelos
+        LineModel::firstOrCreate(['name' => 'X3 30e xDrive', 'line_id' => $x3Line->id], ['year' => 2025]);
+        LineModel::firstOrCreate(['name' => 'X3 M40i', 'line_id' => $x3Line->id], ['year' => 2024]);
+        LineModel::firstOrCreate(['name' => 'Cooper C', 'line_id' => $cooperLine->id], ['year' => 2025]);
+        LineModel::firstOrCreate(['name' => 'Tracker Premier', 'line_id' => $trackerLine->id], ['year' => 2023]);
 
-        $chevrolet = VehicleBrand::create([
-            'name' => 'Chevrolet',
-            'image_path' => 'chevrolet-logo.png'
-        ]);
+        // Carrocerías
+        VehicleBody::firstOrCreate(['name' => 'SUV']);
+        VehicleBody::firstOrCreate(['name' => 'Hatchback']);
+        VehicleBody::firstOrCreate(['name' => 'Sedan']);
+        VehicleBody::firstOrCreate(['name' => 'Coupe']);
 
-        $gmc = VehicleBrand::create([
-            'name' => 'GMC',
-            'image_path' => 'gmc-logo.png'
-        ]);
-
-        // Crear líneas de marca
-        $x3Line = BrandLine::create([
-            'name' => 'X3',
-            'brand_id' => $bmw->id
-        ]);
-
-        $x4Line = BrandLine::create([
-            'name' => 'X4',
-            'brand_id' => $bmw->id
-        ]);
-
-        $cooperLine = BrandLine::create([
-            'name' => 'Cooper',
-            'brand_id' => $mini->id
-        ]);
-
-        $trackerLine = BrandLine::create([
-            'name' => 'Tracker',
-            'brand_id' => $chevrolet->id
-        ]);
-
-        // Crear modelos
-        $x3Model = LineModel::create([
-            'name' => 'X3 30e xDrive',
-            'year' => 2025,
-            'line_id' => $x3Line->id
-        ]);
-
-        $x3M40iModel = LineModel::create([
-            'name' => 'X3 M40i',
-            'year' => 2024,
-            'line_id' => $x3Line->id
-        ]);
-
-        $cooperModel = LineModel::create([
-            'name' => 'Cooper C',
-            'year' => 2025,
-            'line_id' => $cooperLine->id
-        ]);
-
-        $trackerModel = LineModel::create([
-            'name' => 'Tracker Premier',
-            'year' => 2023,
-            'line_id' => $trackerLine->id
-        ]);
-
-        // Crear tipos de carrocería
-        $suv = VehicleBody::create(['name' => 'SUV']);
-        $hatchback = VehicleBody::create(['name' => 'Hatchback']);
-        $sedan = VehicleBody::create(['name' => 'Sedan']);
-        $coupe = VehicleBody::create(['name' => 'Coupe']);
-
-        // Crear concesionarios
-        $vecsaHidalgo = Dealership::create([
-            'name' => 'VECSA Hidalgo',
-            'location' => 'hidalgo',
-            'description' => 'Concesionario BMW en Pachuca, Hidalgo'
-        ]);
-
-        $vecsaPuebla = Dealership::create([
-            'name' => 'VECSA Puebla',
-            'location' => 'puebla',
-            'description' => 'Concesionario BMW en Puebla, Puebla'
-        ]);
+        // Concesionarios
+        Dealership::firstOrCreate(['name' => 'VECSA Hidalgo'], ['location' => 'hidalgo', 'description' => 'Concesionario BMW en Pachuca, Hidalgo']);
+        Dealership::firstOrCreate(['name' => 'VECSA Puebla'], ['location' => 'puebla', 'description' => 'Concesionario BMW en Puebla, Puebla']);
 
         $this->command->info('Datos de vehículos creados exitosamente!');
     }
-} 
+}
